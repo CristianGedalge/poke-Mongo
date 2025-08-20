b61d415f8de9cd8c21aaf47195d77a798d81767c
Cristian_Gedalge <gedalge.cristian22@gmail.com>
2025-08-20 09:15:57 -0400

    nombre de la db

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,9 @@ import { joiSchema } from './config/joi.Schema.Validation';
     ServeStaticModule.forRoot({
       rootPath:join(__dirname,'..','public')
     }),
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(process.env.MONGODB,{
+      dbName:'Pokemondb'
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
